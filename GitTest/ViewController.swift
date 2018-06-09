0b035fb8ca8b61284f7013b87911656d73209d3e
Andrew Courey <Andrew@andrews-macbook-pro.local>
2018-06-09 11:23:14 -0400

    Add for loop

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+
+        for i in 0..101 {
+            print(i)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
